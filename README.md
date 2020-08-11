462a108315100ab82cfd931658ae08f00cb86fd4
Florian Heinze <florian.heinze@sandstorm.de>
2020-08-11 23:42:44 +0200

    TASK: Readme and prettier

--- README.md
+++ README.md
@@ -1,19 +1,108 @@
 # Sandstorm.CookieCutter
 
+**THIS PACKAGE IS STILL WIP ;) FEEDBACK IS HIGHLY WELCOME.**
+
 This Neos package provides a general approach for blocking elements like script tags and iframes before the markup reaches the browser and therefore provides a general approach for blocking cookies or other concepts of tracking user behaviour. It also provides a UI in the browser for displaying a cookie-consent and partially unblocking groups of elements.
 
-**This is what you can do so far:**
+## Features
 
-* verbosely block elements on the server before the markup is send to the client
-* choose between two blocking approaches:
-    * block everything and whitelist exceptions (recommended)
+* helpers to block elements (scripts and iframes) before the markup is send to the client
+* blocking modes:
+    * whitelist elements to not be blocked
     * blacklist elements to be blocked
-* block elements from all packages, without the need to customize or override
-* block content created by editors, e.g. if they copy pasted HTML snippetss
-* enable/disable groups of elements through a cookie consent in the frontend
-* localize the frontend using Settings.yaml and Xliff or directly use content translated by your editors
-* start with useful defaults concerning configuration and styling
-* easily customize when needed
+* grouping of elements
+* patterns to target elements in markup
+* blocking of HTML snippets created by the editor (Neos.NodeTypes.Html:Html)
+* cookie-consent (provided by Klaro.js) to block/unblock grouped elements 
+* localization support
+* useful default config and styling
+* SCSS to customize cookie-consent styling
 
+## Instalation
 
-WIP
+`composer require sandstorm/cookiecutter`
+
+## Basic Setup
+
+### STEP 1: Adding the consent-modal
+
+In your `Overrides.fusion` or `Root.fusion` add
+
+```
+prototype(Neos.Neos:Page) {
+	// This adds the javascript and css needed for the cookie-consent
+	head.javascripts.cookieCutterConsent = Sandstorm.CookieCutter:Consent
+}
+```
+
+This will add the needed js and css to your page. If you reload the page you should see the consent-modal.
+
+### STEP 2: Blocking everything
+
+```
+prototype(Neos.Neos:Page) {
+	// This adds the javascript and css needed for the cookie-consent
+	head.javascripts.cookieCutterConsent = Sandstorm.CookieCutter:Consent
+	
+	// This will block all iframes & script tags
+	@process.blockIframes = ${CookieCutter.blockIframes(value, !node.context.inBackend)}
+	@process.blockScripts = ${CookieCutter.blockScripts(value, !node.context.inBackend)}
+}
+```
+
+Open the console of your browser inspector an type `klaro.show()`. Make sure all switches are turned off and reload your page again.
+
+As the default behaviour we will block all iframes and scripts. This way nothing slips through e.g. when you install a new plugin. As a developper you should allways check if can trust the markup added by a plugin. 
+
+Don't worry, we provide some tools to make it easiert for you to configure ;)
+
+Now that your page probably looks broken, let's try to fix it ;)
+
+### STEP 3: Never block your own javascript
+
+You might have some scripts that you never want to be blocked because your page would not be usable at all. They are often called `main.js`, `app.js`, `index.js`, ...
+
+```
+Neos.Fusion:Tag {
+	tagName = "script"
+   attrbutes.src = "resource://Vendor.Example/Public/JavaScript/index.js"
+   @process.neverBlock = ${CookieCutter.neverBlockScripts(value)}
+}	
+```
+
+This Eel helper will add `data-never-block` attribute to you script tag. These tags will be ignored by `CookieCutter.blockScripts()`.
+
+### STEP 4: Create groups, purposes and add elements
+
+In `Configuration/` create a `Settings.CookieCutter.yaml`.
+
+```yaml
+Sandstorm:
+  CookieCutter:
+    purposes:
+      mediaembeds: Media Embeds
+    groups:
+      media:
+      	title: Bar
+      	purposes:
+      	  - mediaembeds
+      	description: Some bar description
+    elements:
+    	Sandstorm:
+  CookieCutter:
+    purposes:
+      mediaembeds: Media Embeds
+    groups:
+      media:
+        title: Bar
+        purposes:
+          - mediaembeds
+        description: Some bar description
+    elements:
+      "https://www.youtube.com/embed/":
+        type: iframe
+        block: true
+        group: media
+```
+
+Now all tags containing `"https://www.youtube.com/embed/"` will be blocked and added to the group `media`. Reload you page, open the consent modal -> you schould see a new switch.
--- Resources/Private/JavaScript/klaroConfigTemplate.js
+++ Resources/Private/JavaScript/klaroConfigTemplate.js
@@ -23,204 +23,189 @@
 // show in the browser, even after reloading!
 
 var $php_replaced__apps,
-    $php_replaced__translations,
-    $php_replaced__privacyPolicyUrl,
-    $php_replaced__cookieDomain,
-    $php_replaced__cookieExpiresAfterDays,
-    $php_replaced__storageMethod,
-    $php_replaced__cookieName,
-    $php_replaced__mustConsent,
-    $php_replaced__default,
-    $php_replaced__acceptAll,
-    $php_replaced__hideDeclineAll;
+  $php_replaced__translations,
+  $php_replaced__privacyPolicyUrl,
+  $php_replaced__cookieDomain,
+  $php_replaced__cookieExpiresAfterDays,
+  $php_replaced__storageMethod,
+  $php_replaced__cookieName,
+  $php_replaced__mustConsent,
+  $php_replaced__default,
+  $php_replaced__acceptAll,
+  $php_replaced__hideDeclineAll;
 var $php_replaced__handleConsentOptions = {
-    message: "Message",
-    messageClass: "messageClass",
+  message: "Message",
+  messageClass: "messageClass",
 };
 
 /* PHP_REMOVE_END */
 
 (function () {
-    window.klaroConfig = {
-        // IMPORTANT: we disable the language handling of klaro because
-        // we want to use translations provided the Neos-way
-        lang: "all",
+  window.klaroConfig = {
+    // IMPORTANT: we disable the language handling of klaro because
+    // we want to use translations provided the Neos-way
+    lang: "all",
 
-        // How Klaro should store the user's preferences. It can be either 'cookie'
-        // (the default) or 'localStorage'.
-        storageMethod: $php_replaced__storageMethod,
+    // How Klaro should store the user's preferences. It can be either 'cookie'
+    // (the default) or 'localStorage'.
+    storageMethod: $php_replaced__storageMethod,
 
-        // You can customize the name of the cookie that Klaro uses for storing
-        // user consent decisions. If undefined, Klaro will use 'klaro'.
-        cookieName: $php_replaced__cookieName,
+    // You can customize the name of the cookie that Klaro uses for storing
+    // user consent decisions. If undefined, Klaro will use 'klaro'.
+    cookieName: $php_replaced__cookieName,
 
-        // You can also set a custom expiration time for the Klaro cookie.
-        // By default, it will expire after 120 days.
-        cookieExpiresAfterDays: $php_replaced__cookieExpiresAfterDays,
+    // You can also set a custom expiration time for the Klaro cookie.
+    // By default, it will expire after 120 days.
+    cookieExpiresAfterDays: $php_replaced__cookieExpiresAfterDays,
 
-        // You can change to cookie domain for the consent manager itself.
-        // Use this if you want to get consent once for multiple matching domains.
-        // If undefined, Klaro will use the current domain.
-        cookieDomain: $php_replaced__cookieDomain,
+    // You can change to cookie domain for the consent manager itself.
+    // Use this if you want to get consent once for multiple matching domains.
+    // If undefined, Klaro will use the current domain.
+    cookieDomain: $php_replaced__cookieDomain,
 
-        // Put a link to your privacy policy here (relative or absolute).
-        privacyPolicy: $php_replaced__privacyPolicyUrl,
+    // Put a link to your privacy policy here (relative or absolute).
+    privacyPolicy: $php_replaced__privacyPolicyUrl,
 
-        // Defines the default state for applications (true=enabled by default).
-        default: $php_replaced__default,
+    // Defines the default state for applications (true=enabled by default).
+    default: $php_replaced__default,
 
-        // If "mustConsent" is set to true, Klaro will directly display the consent
-        // manager modal and not allow the user to close it before having actively
-        // consented or declines the use of third-party apps.
-        mustConsent: $php_replaced__mustConsent,
+    // If "mustConsent" is set to true, Klaro will directly display the consent
+    // manager modal and not allow the user to close it before having actively
+    // consented or declines the use of third-party apps.
+    mustConsent: $php_replaced__mustConsent,
 
-        // Show "accept all" to accept all apps instead of "ok" that only accepts
-        // required and "default: true" apps
-        acceptAll: $php_replaced__acceptAll,
+    // Show "accept all" to accept all apps instead of "ok" that only accepts
+    // required and "default: true" apps
+    acceptAll: $php_replaced__acceptAll,
 
-        // replace "decline" with cookie manager modal
-        hideDeclineAll: $php_replaced__hideDeclineAll,
+    // replace "decline" with cookie manager modal
+    hideDeclineAll: $php_replaced__hideDeclineAll,
 
-        translations: {
-            all: $php_replaced__translations,
-        },
+    translations: {
+      all: $php_replaced__translations,
+    },
 
-        apps: $php_replaced__apps,
-    };
+    apps: $php_replaced__apps,
+  };
 
-    function handleConsent(consent, app) {
-        // We nee a stable class to identify messages so we can remove them later.
-        const PERMANENT_MESSAGE_CLASS = "block-them-all-message";
-        // -> see Settings.yaml and/or Config.fusion
-        const handleConsentOptions = $php_replaced__handleConsentOptions;
+  function handleConsent(consent, app) {
+    // We nee a stable class to identify messages so we can remove them later.
+    const PERMANENT_MESSAGE_CLASS = "block-them-all-message";
+    // -> see Settings.yaml and/or Config.fusion
+    const handleConsentOptions = $php_replaced__handleConsentOptions;
 
-        if (consent) {
-            removeMessagesForConsentGroup(app.name);
-            unhideIframesForGroup(app.name);
-        } else {
-            let options = null;
-            document
-                .querySelectorAll(`*[data-name="${app.name}"]`)
-                .forEach((element) => {
-                    try {
-                        // more robust handling of maybe broken JSON
-                        options = JSON.parse(element.dataset.options);
-                    } catch (e) {
-                        // Do nothing
-                    }
+    if (consent) {
+      removeMessagesForConsentGroup(app.name);
+      unhideIframesForGroup(app.name);
+    } else {
+      let options = null;
+      document
+        .querySelectorAll(`*[data-name="${app.name}"]`)
+        .forEach((element) => {
+          try {
+            // more robust handling of maybe broken JSON
+            options = JSON.parse(element.dataset.options);
+          } catch (e) {
+            // Do nothing
+          }
 
-                    // 1. We set default values
-                    let message =
-                        handleConsentOptions.message || "NO MESSAGE CONFIGURED";
-                    let messageClass = buildMessageClass(
-                        handleConsentOptions.messageClass
-                    );
-                    let messagePosition = "before";
-                    let targetElements = [];
+          // 1. We set default values
+          let message = handleConsentOptions.message || "NO MESSAGE CONFIGURED";
+          let messageClass = buildMessageClass(
+            handleConsentOptions.messageClass
+          );
+          let messagePosition = "before";
+          let targetElements = [];
 
-                    if (element.tagName === "IFRAME") {
-                        targetElements = [element];
-                    }
+          if (element.tagName === "IFRAME") {
+            targetElements = [element];
+          }
 
-                    // 2. We override some, if options are present
-                    if (options) {
-                        message = options.message || message;
-                        messageClass = options.messageClass
-                            ? buildMessageClass(options.messageClass)
-                            : messageClass;
-                        messagePosition = options.messagePosition
-                            ? options.messagePosition
-                            : messagePosition;
-                        targetElements = options.target
-                            ? Array.from(
-                                  document.querySelectorAll(options.target)
-                              )
-                            : targetElements;
-                    }
+          // 2. We override some, if options are present
+          if (options) {
+            message = options.message || message;
+            messageClass = options.messageClass
+              ? buildMessageClass(options.messageClass)
+              : messageClass;
+            messagePosition = options.messagePosition
+              ? options.messagePosition
+              : messagePosition;
+            targetElements = options.target
+              ? Array.from(document.querySelectorAll(options.target))
+              : targetElements;
+          }
 
-                    if (targetElements.length) {
-                        targetElements.forEach((element) =>
-                            addMessage(
-                                element,
-                                message,
-                                app,
-                                messagePosition,
-                                messageClass
-                            )
-                        );
-                    }
-                });
-        }
-        function removeMessagesForConsentGroup(group) {
-            const messageSelector = `.${PERMANENT_MESSAGE_CLASS}[data-name="${group}"]`;
-            document.querySelectorAll(messageSelector).forEach((item) => {
-                item.remove();
-            });
-        }
-
-        function unhideIframesForGroup(group) {
-            const iframeSelector = `iframe[data-name="${group}"]`;
-            document.querySelectorAll(iframeSelector).forEach((item) => {
-                item.style.display = "block";
-            });
-        }
-
-        function buildMessageClass(messageClass) {
-            if (messageClass && messageClass !== PERMANENT_MESSAGE_CLASS) {
-                return `${PERMANENT_MESSAGE_CLASS} ${handleConsentOptions.messageClass}`;
-            }
-            return PERMANENT_MESSAGE_CLASS;
-        }
-
-        function addMessage(
-            targetElement,
-            message,
-            app,
-            messagePosition,
-            messageClass
-        ) {
-            const newElement = document.createElement("div");
-            const classNames = messageClass.split(" ");
-            const innerClassNames = classNames.map(
-                (className) => className + "__inner"
+          if (targetElements.length) {
+            targetElements.forEach((element) =>
+              addMessage(element, message, app, messagePosition, messageClass)
             );
-            newElement.innerHTML = `<div class="${innerClassNames.join(
-                " "
-            )}">${message.replace("{group}", app.title)}</div>`;
-            newElement.onclick = function () {
-                klaro.show();
-            };
-            newElement.style.cursor = "pointer";
-            newElement.dataset.name = app.name;
-
-            if (messageClass) {
-                newElement.classList.add(...classNames);
-            }
-
-            switch (messagePosition) {
-                case "before": {
-                    targetElement.parentElement.insertBefore(
-                        newElement,
-                        targetElement
-                    );
-                    break;
-                }
-                case "after": {
-                    targetElement.parentElement.insertAfter(
-                        newElement,
-                        targetElement
-                    );
-                    break;
-                }
-                case "prepend": {
-                    targetElement.prepend(newElement);
-                    break;
-                }
-                case "append": {
-                    targetElement.append(newElement);
-                    break;
-                }
-            }
-        }
+          }
+        });
     }
+    function removeMessagesForConsentGroup(group) {
+      const messageSelector = `.${PERMANENT_MESSAGE_CLASS}[data-name="${group}"]`;
+      document.querySelectorAll(messageSelector).forEach((item) => {
+        item.remove();
+      });
+    }
+
+    function unhideIframesForGroup(group) {
+      const iframeSelector = `iframe[data-name="${group}"]`;
+      document.querySelectorAll(iframeSelector).forEach((item) => {
+        item.style.display = "block";
+      });
+    }
+
+    function buildMessageClass(messageClass) {
+      if (messageClass && messageClass !== PERMANENT_MESSAGE_CLASS) {
+        return `${PERMANENT_MESSAGE_CLASS} ${handleConsentOptions.messageClass}`;
+      }
+      return PERMANENT_MESSAGE_CLASS;
+    }
+
+    function addMessage(
+      targetElement,
+      message,
+      app,
+      messagePosition,
+      messageClass
+    ) {
+      const newElement = document.createElement("div");
+      const classNames = messageClass.split(" ");
+      const innerClassNames = classNames.map(
+        (className) => className + "__inner"
+      );
+      newElement.innerHTML = `<div class="${innerClassNames.join(
+        " "
+      )}">${message.replace("{group}", app.title)}</div>`;
+      newElement.onclick = function () {
+        klaro.show();
+      };
+      newElement.style.cursor = "pointer";
+      newElement.dataset.name = app.name;
+
+      if (messageClass) {
+        newElement.classList.add(...classNames);
+      }
+
+      switch (messagePosition) {
+        case "before": {
+          targetElement.parentElement.insertBefore(newElement, targetElement);
+          break;
+        }
+        case "after": {
+          targetElement.parentElement.insertAfter(newElement, targetElement);
+          break;
+        }
+        case "prepend": {
+          targetElement.prepend(newElement);
+          break;
+        }
+        case "append": {
+          targetElement.append(newElement);
+          break;
+        }
+      }
+    }
+  }
 })();
